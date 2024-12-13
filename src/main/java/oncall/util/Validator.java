4e7d907e093eadf02a167c78bae7c0557f8caca5
threepebbles <threepebbles@naver.com>
2024-12-13 16:56:02 +0900

    feat(Validator): catch하는 예외 구체화, 에러 메세지 수정

--- src/main/java/oncall/util/Validator.java
+++ src/main/java/oncall/util/Validator.java
@@ -4,7 +4,7 @@ public class Validator {
     public static void validateInteger(String s) {
         try {
             Integer.parseInt(s);
-        } catch (Exception e) {
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 정수가 아닙니다.");
         }
     }
